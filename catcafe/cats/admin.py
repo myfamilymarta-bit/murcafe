413cb29ad445f5623d6c997e1eb40491fa348226
myfamilymarta-bit <myfamily.marta@gmail.com>
2025-11-08 14:24:11 +0100

    Register Cat and related models in admin site

--- catcafe/cats/admin.py
+++ catcafe/cats/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cat, Staff, StaffReview, MenuItem, ContactMessage
 
-# Register your models here.
+admin.site.register(Cat)
+admin.site.register(MenuItem)
+admin.site.register(StaffReview)
+admin.site.register(Staff)
+admin.site.register(ContactMessage)
